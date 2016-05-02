17565c1a2fc8097c78bf9cf6e2df68ffd59954d2
Ryan Vennell <ryan.vennell@gmail.com>
2016-05-01 22:44:52 -0500

    Adding a few dependencies

--- enliven.go
+++ enliven.go
@@ -8,9 +8,13 @@ import (
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+// This is an accesible
 var enliven Enliven
 
 // EnlivenMiddleware is the interface making for any middleware added to enliven
